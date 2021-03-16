23e6ac10b7cd3a1708172bc7c8d5ed40b98d9465
Momcilo Majic <momcilo.majic@elektrobit.com>
2021-03-16 22:00:34 +0100

    Updated

--- src/cov19/vaccination/templates/human_template.py
+++ src/cov19/vaccination/templates/human_template.py
@@ -1,24 +1,26 @@
 import jinja2
+import locale 
 
 class HumanTemplate(object):
 	name='human'
 	template_str = '''
 Hi @edomt
 Here is the latest report for {{ date.strftime("%d.%m.%Y") }}
-total_vaccinations: {{ total_number_of_vaccinations }}
-people_fully_vaccinated: {{ fully_vaccinated }}
-people_vaccinated: {{ fully_vaccinated }} + ({{ total_number_of_vaccinations }} - (2*{{ fully_vaccinated }})) = {{ fully_vaccinated + (total_number_of_vaccinations - (2 * fully_vaccinated)) }}
-total_vaccinations_per_hundred: 100 * {{ total_number_of_vaccinations }}/6.804.596 = {{ total_number_of_vaccinations /6804596}}
+total_vaccinations: {{ total_number_of_vaccinations | dots }}
+people_fully_vaccinated: {{ fully_vaccinated | dots }}
+people_vaccinated: {{ fully_vaccinated | dots }} + ({{ total_number_of_vaccinations | dots }} - (2 * {{ fully_vaccinated | dots }})) = {{ (fully_vaccinated + (total_number_of_vaccinations - (2 * fully_vaccinated))) | dots }}
+total_vaccinations_per_hundred: 100 * {{ total_number_of_vaccinations | dots }}/6.804.596 = {{ (total_number_of_vaccinations / 6804596 ) | dots}}
 
 Source: {{ source }} / {{ news_datetime.strftime("%d.%m.%Y, %H:%M") }}
 
 {{ url }}
 
 "{{ sentence }}"
-"In Serbia, according to latest Gov data, there was {{ total_number_of_vaccinations }} vaccinations against Corona virus and {{ fully_vaccinated }} citizens have received second vaccine dose"	
+"In Serbia, according to latest Gov data, there was {{ total_number_of_vaccinations }} vaccinations against Corona virus and {{ fully_vaccinated }} citizens have received second vaccine dose"
 '''
 	def __init__(self):
+# 		env = jinja2.Environment()
+# 		env.filters['commafy'] = lambda v: "{:,}".format(v)
 		self.template = jinja2.Template(HumanTemplate.template_str)
-	
-	def generate(self, result):	
+	def generate(self, result):
 		return self.template.render(result)
